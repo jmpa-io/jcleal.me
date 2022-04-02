adf55bf22911df44fdab5c3678a21da79549099b
Jordan Cleal <mr.jordan.cleal+github@gmail.com>
2022-04-02 13:06:12 +1100

    change resume out directory, so it will be pulled in correctly to the artifact

--- bin/generate-resume.sh
+++ bin/generate-resume.sh
@@ -26,13 +26,20 @@ repo=$(basename "$PWD") \
 docker build . -t "$repo" \
   || die "failed to build Dockerfile for $repo"
 
+# setup out directory.
+dir="public"
+if [[ -d "$dir" ]]; then
+  mkdir -p "$dir" \
+    || die "failed to create $dir"
+fi
+
 # generate resume.
 # https://pandoc.org/demos.html
-file="Resume.pdf"
+file="$dir/Resume.pdf"
 docker run --rm \
   -w /app \
   -v "$PWD:/app" \
-  "$repo" --metadata-file ./resume/metadata.yml ./resume/data.md -o $file \
+  "$repo" --metadata-file ./resume/metadata.yml ./resume/data.md -o "$file" \
   || die "failed to create $file using pandoc"
 
 # chown file.
